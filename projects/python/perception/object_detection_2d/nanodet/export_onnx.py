3d392b78e719e18e0b6747a9691cb1b9726a502a
ManosMpampis <gmp.manos@gmail.com>
2023-01-24 20:13:11 +0200

    update to original nanodet from opendr

--- projects/python/perception/object_detection_2d/nanodet/export_onnx.py
+++ projects/python/perception/object_detection_2d/nanodet/export_onnx.py
@@ -1,4 +1,4 @@
-# Copyright 2020-2023 OpenDR European Project
+# Copyright 2020-2022 OpenDR European Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,30 +13,24 @@
 # limitations under the License.
 
 import argparse
-from opendr.perception.object_detection_2d import NanodetLearner
 from opendr.engine.data import Image
-from opendr.perception.object_detection_2d import draw_bounding_boxes
+from opendr.perception.object_detection_2d import NanodetLearner, draw_bounding_boxes
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--device", help="Device to use (cpu, cuda)", type=str, default="cuda", choices=["cuda", "cpu"])
-    parser.add_argument("--model", help="Model for which a config file will be used", type=str, default="m")
-    parser.add_argument("--path", help="Path to the image that is used for inference", type=str,
-                        default="./predefined_examples/000000000036.jpg")
-    parser.add_argument("--optimize", help="If specified will determine the optimization to be used (onnx, jit)",
-                        type=str, default="", choices=["", "onnx", "jit"])
+    parser.add_argument("--model", help="Model that config file will be used", type=str, default='m')
     args = parser.parse_args()
 
     nanodet = NanodetLearner(model_to_use=args.model, device=args.device)
     nanodet.download("./predefined_examples", mode="pretrained")
     nanodet.load("./predefined_examples/nanodet_{}".format(args.model), verbose=True)
+
     nanodet.download("./predefined_examples", mode="images")
-
-    img = Image.open(args.path)
-
-    if args.optimize != "":
-        nanodet.optimize("./{}/nanodet_{}".format(args.optimize, args.model), optimization=args.optimize)
+    # First read an openDR image from your dataset and run the optimizer:
+    img = Image.open("./predefined_examples/000000000036.jpg")
+    nanodet.optimize("./onnx/nanodet_{}".format(args.model), img, optimization="onnx")
 
     boxes = nanodet.infer(input=img)
 
