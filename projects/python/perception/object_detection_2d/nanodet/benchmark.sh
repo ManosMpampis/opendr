46d64ada1fcb6e3b95ba84e1ea6625ea1e0d617e
manos <gmp.manos@gmail.com>
2023-03-09 13:37:26 +0200

    nothing

--- projects/python/perception/object_detection_2d/nanodet/benchmark.sh
+++ projects/python/perception/object_detection_2d/nanodet/benchmark.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --conf-thresh 0.35 --iou-thresh 0.6 --nms 50 --model m_32 > ./measures.txt
 python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --conf-thresh 0.35 --iou-thresh 0.6 --nms 50 --model vgg_64 >> ./measures.txt
-
