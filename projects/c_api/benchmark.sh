5d577c303e6a05e72ea5368769143d97353c0c2a
manos <gmp.manos@gmail.com>
2023-03-26 04:14:15 +0300

    add new model for bench

--- projects/c_api/benchmark.sh
+++ projects/c_api/benchmark.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-./build/object_detection_2d/nanodet_jit_benchmark ./data/object_detection_2d/nanodet/nanodet_vgg_64_big vgg_64_big ./data/object_detection_2d/nanodet/database/000000000036.jpg 1000 100 >> ./measures.txt
+./build/object_detection_2d/nanodet_jit_benchmark ./data/object_detection_2d/nanodet/nanodet_vgg_64_big vgg_64_big ./data/object_detection_2d/nanodet/database/000000000036.jpg 1000 100 > ./measures.txt
 ./build/object_detection_2d/nanodet_jit_benchmark ./data/object_detection_2d/nanodet/nanodet_vgg_64_big_conv vgg_64_big_conv ./data/object_detection_2d/nanodet/database/000000000036.jpg 1000 100 >> ./measures.txt
 
 ./build/object_detection_2d/nanodet_jit_benchmark ./data/object_detection_2d/nanodet/nanodet_vgg_64_medium vgg_64_medium ./data/object_detection_2d/nanodet/database/000000000036.jpg 1000 100 >> ./measures.txt
--- projects/python/perception/object_detection_2d/nanodet/benchmark.sh
+++ projects/python/perception/object_detection_2d/nanodet/benchmark.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
-#python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --model vgg_64_big > ./measures_pc_gpan_vgg.txt
-#python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --mix --model vgg_64_big >> ./measures_pc_gpan_vgg.txt
-#python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --model vgg_64_big_conv >> ./measures_pc_gpan_vgg.txt
-#python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --mix --model vgg_64_big_conv >> ./measures_pc_gpan_vgg.txt
-#
-#python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --model vgg_64_medium >> ./measures_pc_gpan_vgg.txt
-#python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --mix --model vgg_64_medium >> ./measures_pc_gpan_vgg.txt
-#python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --model vgg_64_medium_conv >> ./measures_pc_gpan_vgg.txt
-#python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --mix --model vgg_64_medium_conv >> ./measures_pc_gpan_vgg.txt
+python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --model vgg_64_big > ./measures.txt
+python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --model vgg_64_big_conv >> ./measures.txt
+python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --mix --model vgg_64_big >> ./measures.txt
+python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --mix --model vgg_64_big_conv >> ./measures.txt
 
-#python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --model vgg_64_small >> ./measures_pc_gpan_vgg.txt
-#python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --mix --model vgg_64_small >> ./measures_pc_gpan_vgg.txt
-python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --model vgg_64_small_conv >> ./measures_pc_gpan_vgg.txt
-#python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --mix --model vgg_64_small_conv >> ./measures_pc_gpan_vgg.txt
+python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --model vgg_64_medium >> ./measures.txt
+python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --model vgg_64_medium_conv >> ./measures.txt
+python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --mix --model vgg_64_medium >> ./measures.txt
+python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --mix --model vgg_64_medium_conv >> ./measures.txt
+
+python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --model vgg_64_small >> ./measures.txt
+python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --model vgg_64_small_conv >> ./measures.txt
+python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --mix --model vgg_64_small >> ./measures.txt
+python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --mix --model vgg_64_small_conv >> ./measures.txt
+
+python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --model vgg_64_very_small >> ./measures.txt
+python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --model vgg_64_very_small_conv >> ./measures.txt
+python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --mix --model vgg_64_very_small >> ./measures.txt
+python3 ./benchmark_demo.py --repetitions 1000 --warmup 100 --mix --model vgg_64_very_small_conv >> ./measures.txt
