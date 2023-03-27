correct half precision of preprocessing and contiguous output for correct pointers
trt runs explicity in fp16 if it was made for fp16, inputs is still in fp32 from onnx

src/opendr/perception/object_detection_2d/nanodet/algorithm/nanodet/inferencer/trt_dep.py
@@ -192,11 +192,9 @@ def do_inference_v2(context, bindings, inputs, outputs, stream):
 
 
 class trt_model():
-    def __init__(self, engine, model_cfg, device="cuda", mix=False):
+    def __init__(self, engine, model_cfg, device="cuda"):
         self.engine = engine
-        self.stream = cuda.Stream()
         self.bindings = [None] * self.engine.num_bindings
-        # self.inputs, self.outputs, self.bindings, self.stream = allocate_buffers(engine)
         self.context = engine.create_execution_context()
 
         nclass = model_cfg["model"]["arch"]["head"]["num_classes"]
@@ -210,23 +208,9 @@ class trt_model():
             out_feature_dim += size
         self.output_shape = (1, int(out_feature_dim), out_cls_reg_dim)
         self.output = torch.zeros(self.output_shape).to(device)
-        if mix:
-            self.output = self.output.half()
-        self.bindings[self.engine.get_binding_index('data')] = self.output.data_ptr()
+        self.bindings[self.engine.get_binding_index('output')] = self.output.data_ptr()
 
-    # def __call__(self, input):
-    #     np.copyto(self.inputs[0].host, input)
-    def __call__(self, input):#, output):
-        import numpy as np
-        # np.copyto(self.inputs[0].host, input)
-        # self.inputs[0].host = input.data_ptr()
-        # preds = do_inference_v2(self.context, bindings=self.bindings, inputs=self.inputs, outputs=self.outputs, stream=self.stream)
-        # return preds[0].reshape(self.output_shape)
-
-        # output = torch.zeros(self.output_shape).cuda()
-        # bindings = [None] * self.engine.num_bindings
-        self.bindings[self.engine.get_binding_index('output')] = input.data_ptr()
-
-        self.context.execute_async_v2(bindings=self.bindings)
-        print("done")
+    def __call__(self, input):
+        self.bindings[self.engine.get_binding_index('data')] = input.data_ptr()
+        self.context.execute_v2(bindings=self.bindings)
         return self.output

src/opendr/perception/object_detection_2d/nanodet/algorithm/nanodet/inferencer/utilities.py
@@ -81,7 +81,7 @@ class Predictor(nn.Module):
         meta["img"] = torch.from_numpy(meta["img"].transpose(2, 0, 1)).to(self.device)
         meta["img"] = divisible_padding(meta["img"], divisible=torch.tensor(32))
 
-        _input = meta["img"]
+        _input = meta["img"].half().contiguous() if self.mix else meta["img"].contiguous()
         _height = torch.tensor(height)
         _width = torch.tensor(width)
         _warp_matrix = torch.from_numpy(meta["warp_matrix"])
@@ -96,13 +96,14 @@ class Predictor(nn.Module):
 
 
 class Postprocessor(nn.Module):
-    def __init__(self, cfg, model, device="cuda", conf_thresh=0.35, iou_thresh=0.6, nms_max_num=100):
+    def __init__(self, cfg, model, device="cuda", conf_thresh=0.35, iou_thresh=0.6, nms_max_num=100, mix=True):
         super(Postprocessor, self).__init__()
         self.cfg = cfg
         self.device = device
         self.conf_thresh = conf_thresh
         self.iou_thresh = iou_thresh
         self.nms_max_num = nms_max_num
+        self.mix = mix
         if self.cfg.model.arch.backbone.name == "RepVGG":
             deploy_config = self.cfg.model
             deploy_config.arch.backbone.update({"deploy": True})
@@ -118,6 +119,8 @@ class Postprocessor(nn.Module):
 
     def forward(self, preds, input, height, width, warp_matrix):
         meta = {"height": height, "width": width, 'img': input, 'warp_matrix': warp_matrix}
+        if self.mix:
+            preds = preds.half()
         res = self.model.head.post_process(preds, meta, conf_thresh=self.conf_thresh, iou_thresh=self.iou_thresh,
                                            nms_max_num=self.nms_max_num)
         return res