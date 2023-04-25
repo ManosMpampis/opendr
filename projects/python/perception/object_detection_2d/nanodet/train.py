52e4279d36a851995267838f18482c5beed26c5d
manos <gmp.manos@gmail.com>
2023-04-25 12:30:08 +0300

    update code

--- projects/python/perception/object_detection_2d/nanodet/train.py
+++ projects/python/perception/object_detection_2d/nanodet/train.py
@@ -21,18 +21,20 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", help="Model for which a config file will be used", type=str, default="vgg_64_very_small") # "temp" "m" "plus_m_320"
     parser.add_argument("--device", help="Device to use (cpu, cuda)", type=str, default="cuda", choices=["cuda", "cpu"])
-    parser.add_argument("--batch-size", help="Batch size to use for training", type=int, default=1)
-    parser.add_argument("--lr", help="Learning rate to use for training", type=float, default=0.01)
+    parser.add_argument("--batch-size", help="Batch size to use for training", type=int, default=512)#256)
+    parser.add_argument("--lr", help="Learning rate to use for training", type=float, default=0.002)
     parser.add_argument("--checkpoint-freq", help="Frequency in-between checkpoint saving and evaluations",
-                        type=int, default=1)
-    parser.add_argument("--n-epochs", help="Number of total epochs", type=int, default=1000)
+                        type=int, default=5)
+    parser.add_argument("--n-epochs", help="Number of total epochs", type=int, default=100)
     parser.add_argument("--resume-from", help="Epoch to load checkpoint file and resume training from",
                         type=int, default=0)
-
+    parser.add_argument("--dataset_path", help="Path to dataset", type=str,
+                        default="/media/manos/hdd/Binary_Datasets/Football/192x192_2pos_36neg")
+    parser.add_argument("--save_dir")
     args = parser.parse_args()
-
+    # "/media/manos/hdd/Binary_Datasets/Football/96x96_1pos_9neg_2"
     dataset_metadata = {
-        "data_root": "/media/manos/hdd/Binary_Datasets/Football/96x96_1pos_9neg",
+        "data_root": args.dataset_path,
         "classes": ["player"],
         "dataset_type": "BINARY_FOOTBALL",
     }
@@ -40,21 +42,19 @@ if __name__ == '__main__':
     classes = dataset_metadata["classes"]
     dataset_type = dataset_metadata["dataset_type"]
 
-    dataset = XMLBasedDataset(root=f'{data_root}/1image', dataset_type=dataset_type, images_dir='images',
+    dataset = XMLBasedDataset(root=f'{data_root}/train', dataset_type=dataset_type, images_dir='images',
                               annotations_dir='annotations', classes=classes)
 
-    val_dataset = XMLBasedDataset(root=f'{data_root}/1image', dataset_type=dataset_type, images_dir='images',
+    val_dataset = XMLBasedDataset(root=f'{data_root}/eval', dataset_type=dataset_type, images_dir='images',
                                   annotations_dir='annotations', classes=classes)
 
     # nanodet = NanodetLearner(model_to_use=args.model, iters=args.n_epochs, lr=args.lr, batch_size=args.batch_size,
     #                          checkpoint_after_iter=args.checkpoint_freq, checkpoint_load_iter=args.resume_from,
     #                          device=args.device)
 
-    nanodet = NanodetLearner(model_to_use=args.model, iters=None, lr=None, batch_size=1,
-                             checkpoint_after_iter=1, checkpoint_load_iter=0,
+    nanodet = NanodetLearner(model_to_use=args.model, iters=args.n_epochs, lr=args.lr, batch_size=args.batch_size,
+                             checkpoint_after_iter=args.checkpoint_freq, checkpoint_load_iter=args.resume_from,
                              device="cuda", model_log_name=f"{args.model}")
 
-    # nanodet.download("./predefined_examples", mode="pretrained")
-    # nanodet.load(f"./predefined_examples/nanodet_{args.model}", verbose=True)
     nanodet.fit(dataset, val_dataset, logging=True)
     nanodet.save(f"./saved/nanodet_{args.model}")
